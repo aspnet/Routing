f429468c7eb48eb95dd8091b79e2039171cc9f8e
LBi Jeroen Slor <jeroen.slor@lbi.com>
2015-01-30 22:35:38 +0100

    Added configure options. Added default default addoptions call

--- src/Microsoft.AspNet.Routing/RoutingServices.cs
+++ src/Microsoft.AspNet.Routing/RoutingServices.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
+using System;
+using System.Threading.Tasks;
 using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 
@@ -8,10 +9,18 @@ namespace Microsoft.AspNet.Routing
 {
     public static class RoutingServices
     {
-        public static IServiceCollection AddRouting(this IServiceCollection services, IConfiguration config = null)
+        public static IServiceCollection AddRouting(this IServiceCollection services, IConfiguration config = null, Action<RouteOptions> configureOptions = null)
         {
             var describe = new ServiceDescriber(config);
+
+            services.AddOptions(config);
             services.TryAdd(describe.Transient<IInlineConstraintResolver, DefaultInlineConstraintResolver>());
+
+            if (configureOptions != null)
+            {
+                services.Configure(configureOptions);
+            }
+
             return services;
         }
     }
