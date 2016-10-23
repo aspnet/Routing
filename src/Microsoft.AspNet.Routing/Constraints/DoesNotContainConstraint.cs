8194bf8cd892d86d939eb61f9eef04e2eaea3a4b
Ilya Chernomordik <ilya-git@users.noreply.github.com>
2016-10-23 10:59:16 +0200

    Update DoesNotContainConstraint.cs

--- src/Microsoft.AspNet.Routing/Constraints/DoesNotContainConstraint.cs
+++ src/Microsoft.AspNet.Routing/Constraints/DoesNotContainConstraint.cs
@@ -1,13 +1,30 @@
-public class DoesNotContainConstraint : IRouteConstraint
-    {
-        private readonly string m_Substring;
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-        public DoesNotContainConstraint(string substring)
+using System;
+using System.Linq;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Microsoft.AspNet.Routing.Constraints
+{
+    /// <summary>
+    /// Constrains a route parameter to not contain any of the input strings.
+    /// </summary>
+    public class DoesNotContainConstraint : IRouteConstraint
+    {
+        private readonly string[] m_ExcludedStrings;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DoesNotContainConstraint" /> class.
+        /// </summary>
+        public DoesNotContainConstraint(params string[] excludedStrings)
         {
-            m_Substring = substring;
+            m_ExcludedStrings = excludedStrings;
         }
 
-        public bool Match(HttpContext httpContext, IRouter route, string routeKey, IDictionary<string, object> values, RouteDirection routeDirection)
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
         {
             if (httpContext == null)
             {
@@ -31,14 +48,14 @@ public class DoesNotContainConstraint : IRouteConstraint
 
             object routeValue;
 
-            if (values.TryGetValue(routeKey, out routeValue)
-                && routeValue != null)
+            if (values.TryGetValue(routeKey, out routeValue) && routeValue != null)
             {
                 string parameterValueString = Convert.ToString(routeValue, CultureInfo.InvariantCulture);
 
-                return !parameterValueString.Contains(m_Substring);
+                return m_ExcludedStrings.All(s => !parameterValueString.Contains(s));
             }
 
             return true;
         }
     }
+}
