fd4cc185472a2ed83f7e9850f7b80193f161df62
Jass Bagga <jbagga93@live.ca>
2017-09-11 11:04:04 -0700

    Startup experience

---
+++ src/Microsoft.AspNetCore.Dispatcher/DispatcherMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.AspNetCore.Dispatcher
+{
+    public static class DispatcherMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseDispatcher(this IApplicationBuilder builder)
+        {
+            builder.Properties.Add("Dispatcher", true);
+            return builder.UseMiddleware<DispatcherMiddleware>();
+        }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Dispatcher/EndpointMiddleware.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.AspNetCore.Dispatcher
+{
+    public class EndpointMiddleware
+    {
+        private RequestDelegate _next;
+
+        public EndpointMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var feature = context.Features.Get<IDispatcherFeature>();
+            await feature.RequestDelegate(context);
+            await _next(context);
+        }
+    }
+}
