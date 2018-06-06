f6168a3b4ad3dc4b871c961812cc08fa40343c5c
James Newton-King <james@newtonking.com>
2018-06-06 14:05:19 +1200

    Add DispatcherDatasource to abstractions

---
+++ src/Microsoft.AspNetCore.Routing.Abstractions/IEndpointCollectionProvider.cs
@@ -0,0 +1,15 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+
+namespace Microsoft.AspNetCore.Routing
+{
+    public interface IEndpointCollectionProvider
+    {
+        IReadOnlyList<Endpoint> Endpoints { get; }
+
+        IChangeToken ChangeToken { get; }
+    }
+}
