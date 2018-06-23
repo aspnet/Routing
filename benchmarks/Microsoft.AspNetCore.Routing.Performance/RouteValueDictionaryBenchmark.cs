c21cadf9c3f4fbe935095f87838c1a48650e8967
Ryan Nowak <nowakra@gmail.com>
2018-06-23 15:08:40 -0700

    Add benchmarks for RVD
    There are the scenarios that are critical for URL matching performance.

---
+++ benchmarks/Microsoft.AspNetCore.Routing.Performance/RouteValueDictionaryBenchmark.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using BenchmarkDotNet.Attributes;
+
+namespace Microsoft.AspNetCore.Routing
+{
+    public class RouteValueDictionaryBenchmark
+    {
+        [Benchmark]
+        public RouteValueDictionary AddSingleItem()
+        {
+            var dictionary = new RouteValueDictionary();
+            dictionary.Add("action", "Index");
+            return dictionary;
+        }
+
+        [Benchmark]
+        public RouteValueDictionary AddThreeItems()
+        {
+            var dictionary = new RouteValueDictionary();
+            dictionary.Add("action", "Index");
+            dictionary.Add("controller", "Home");
+            dictionary.Add("id", "15");
+            return dictionary;
+        }
+
+        [Benchmark]
+        public RouteValueDictionary SetSingleItem()
+        {
+            var dictionary = new RouteValueDictionary();
+            dictionary.Add("action", "Index");
+            return dictionary;
+        }
+
+        [Benchmark]
+        public RouteValueDictionary SetThreeItems()
+        {
+            var dictionary = new RouteValueDictionary();
+            dictionary.Add("action", "Index");
+            dictionary.Add("controller", "Home");
+            dictionary.Add("id", "15");
+            return dictionary;
+        }
+    }
+}
