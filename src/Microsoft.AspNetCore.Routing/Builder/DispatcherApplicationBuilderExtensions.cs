59f064df8694c58fdd284d2263ed197d74d97e71
James Newton-King <james@newtonking.com>
2018-07-14 18:11:48 +1200

    PR feedback

--- src/Microsoft.AspNetCore.Routing/Builder/DispatcherApplicationBuilderExtensions.cs
+++ src/Microsoft.AspNetCore.Routing/Builder/DispatcherApplicationBuilderExtensions.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Internal;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Microsoft.AspNetCore.Builder
 {
