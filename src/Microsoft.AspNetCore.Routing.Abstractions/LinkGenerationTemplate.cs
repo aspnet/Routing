4496316146e163c9a075de6c8f2a0a914e90dd3d
Kiran Challa <kichalla@users.noreply.github.com>
2018-08-02 15:59:34 -0700

    updated docs

--- src/Microsoft.AspNetCore.Routing.Abstractions/LinkGenerationTemplate.cs
+++ src/Microsoft.AspNetCore.Routing.Abstractions/LinkGenerationTemplate.cs
@@ -3,13 +3,27 @@
 
 namespace Microsoft.AspNetCore.Routing
 {
+    /// <summary>
+    /// Defines a contract to generate a URL from a template.
+    /// </summary>
     public abstract class LinkGenerationTemplate
     {
+        /// <summary>
+        /// Generates a URL with an absolute path.
+        /// </summary>
+        /// <param name="values">An object that contains route values.</param>
+        /// <returns>The generated absolute URL.</returns>
         public string MakeUrl(object values)
         {
             return MakeUrl(values, options: null);
         }
 
+        /// <summary>
+        /// Generates a URL with an absolute path.
+        /// </summary>
+        /// <param name="values">An object that contains route values.</param>
+        /// <param name="options">The <see cref="LinkOptions"/>.</param>
+        /// <returns>The generated absolute URL.</returns>
         public abstract string MakeUrl(object values, LinkOptions options);
     }
 }
