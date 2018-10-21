e55a32ace33eb0fc51fc5cb801ad5e44f543a9b2
Gert Driesen <gert.driesen@telenet.be>
2018-10-21 18:09:10 +0200

    Remove unused import.

--- src/Microsoft.AspNetCore.Routing/DefaultEndpointDataSource.cs
+++ src/Microsoft.AspNetCore.Routing/DefaultEndpointDataSource.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
