0171efc40523ff341515446ea5a1a1471d0cc9eb
James Newton-King <james@newtonking.com>
2018-11-23 08:28:08 +1300

    Fix build

--- samples/RoutingSandbox/UseEndpointRoutingStartup.cs
+++ samples/RoutingSandbox/UseEndpointRoutingStartup.cs
@@ -1,7 +1,16 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Internal;
+using Microsoft.Extensions.DependencyInjection;
 using RoutingSandbox.Framework;
 
 namespace RoutingSandbox
